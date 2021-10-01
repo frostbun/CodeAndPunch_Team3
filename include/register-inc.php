1b644f289c56848296e7186cc19463b14cc96871
Nguyễn Sơn Lộc <nguyensonloc@MacBook-Pro-cua-Nguyen-2.local>
2021-10-01 20:40:02 +0700

    sua lai 1 register-inc.php va login-inc.php

--- include/register-inc.php
+++ include/register-inc.php
@@ -1,52 +1,52 @@
 <?php
-require "database.php";
-if (isset($_POST['submit'])) {
-  $username = $_POST['username'];
-  $password= $_POST['password'];
-  $passRewrite = $_POST['ConfirmPassword'];
-  if (empty($username) || empty($password || empty($passRewrite))) {
-    header("Location: ../register.php?error=emptyFields");
-    exit();
-  } else if (!preg_match("/^[a-zA-Z0-9]*/","$username")) {
-    header("Location: ../register.php?error=invalidUsername");
-    exit();
-  } else if ($password != $passRewrite){
-    header("Location: ../register.php?error=passwordDoNotMatch");
-    exit();
-  } else {
-    $sql = "SELECT username FROM User WHERE username=?";
-    $statement = mysqli_stmt_init($connect);
-    if (!mysqli_stmt_prepare($sql,$statement)) {
-      header("Location: ../register.php?error=sqlError");
+  if (isset($_POST['submit'])) {
+    //Add database connection
+    require 'database.php';
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $confirmPass = $_POST['ConfirmPassword'];
+
+    if (empty($username) || empty($password) || empty($confirmPass)  ) {
+      header("Location: ../register.php?error=Emptyfields&username=".$username);
       exit();
-    } else {
-      mysqli_stmt_bind_param($statement,"s",$username);
-      mysqli_stmt_execute($statement);
-      mysqli_stmt_store_result($statement);
-      $rowCount=mysqli_stmt_num_rows($statement);
-      if ($rowCount > 0 ) {
-        header("Location: ../register.php?error=usernameTaken");
-        exit();
-      } else {
-        $sql = "INSERT INTO User (username,password) VALUES (?,?)";
-        $statement=mysqli_stmt_init($connect);
-        if (!mysqli_stmt_prepare($sql,$statement)) {
+    } else if (!preg_match("/^[a-zA-Z0-9]*/","$username")) {
+      header("Location: ../register.php?error=invalidUsername&username=".$username);
+      exit();
+    } else if ($password != $confirmPass) {
+      header("Location: ../register.php?error=passwordDoNotMatch&username=".$username);
+      exit();
+    } else  {
+        $sql = "SELECT username FROM User WHERE username=?";
+        $statement = mysqli_stmt_init($connect);
+        if (!mysqli_stmt_prepare($statement,$sql)) {
           header("Location: ../register.php?error=sqlError");
           exit();
         } else {
-          mysqli_stmt_bind_param($statement,"ss",$username);
+          mysqli_stmt_bind_param($statement,"s",$username);
           mysqli_stmt_execute($statement);
-          header("Location: ../register.php?succes=registered");
-          exit();
-        }
-      }
-    }
-  }
-  mysqli_stmt_close($statement);
-  mysqli_close($connect);
-} else {
-  header("Location: ../register.php?error=accessForbiden");
-  exit();
-}
+          mysqli_stmt_store_result($statement);
+          $rowCount = mysqli_stmt_num_rows($statement);
 
- ?>
+          if ($rowCount > 0) {
+            header("Location: ../register.php?error=usernameTaken");
+            exit();
+          } else {
+            $sql = "INSERT INTO User (username,password) VALUES (?,?)";
+            $statement = mysqli_stmt_init($connect);
+            if (!mysqli_stmt_prepare($statement,$sql)) {
+                header("Location: ../register.php?error=sqlError");
+                exit();
+              } else {
+                  $hashedPass = password_hash($password,PASSWORD_DEFAULT);
+                  mysqli_stmt_bind_param($statement,"ss",$username,$hashedPass);
+                  mysqli_stmt_execute($statement);
+                  header("Location: ../register.php?succes=registered");
+                  exit();
+              }
+            }
+          }
+        }
+        mysqli_stmt_close($statement);
+        mysqli_close($connect);
+      }
+?>
