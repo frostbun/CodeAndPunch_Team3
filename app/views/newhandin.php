f8c1a5fc6b81912f0a73a4b7f4844a8849d1bd07
Manh Nguyen <manhnb1911@gmail.com>
2021-10-14 23:24:46 +0700

    fix loi upload bai tap chi nhan file txt

--- app/views/newhandin.php
+++ app/views/newhandin.php
@@ -3,7 +3,7 @@
 <div class="container text-center" style="width: 30%">
     <h1>Submit homework</h1>
     <form action="/upload/newhandin/<?=$data["id"]?>" method="POST" enctype= "multipart/form-data">
-        <input class="form-control-file mb-2" type="file" name="file" accept=".txt">
+        <input class="form-control-file mb-2" type="file" name="file" accept=".pdf, .doc, .docx, .txt">
         <p class="text-danger"><?=$data["message"]?></p>
         <button class="btn btn-primary" type="submit" name="submit">Upload</button>
     </form>
