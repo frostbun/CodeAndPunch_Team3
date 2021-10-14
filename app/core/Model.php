d0e8b762f85956fe8ee70497c7a6569494c4daf6
Manh Nguyen <manhnb1911@gmail.com>
2021-10-14 21:13:43 +0700

    try deploy

--- app/core/Model.php
+++ app/core/Model.php
@@ -2,10 +2,10 @@
     class Model {
 
         public static function connect() {
-            $host = "localhost";
-            $user = "ehc";
-            $pass = "ehcteam3";
-            $data = "Classroom";
+            $host = $_ENV["DB_HOST"];
+            $user = $_ENV["DB_USER"];
+            $pass = $_ENV["DB_PASS"];
+            $data = $_ENV["DB_DATA"];
             $db =  new mysqli($host, $user, $pass, $data) or die("There was a problem connecting to the database!");
             return $db;
         }
