72c5aff86f8b54884d29567d706ba9beec411f64
Manh Nguyen <manhnb1911@gmail.com>
2021-10-24 00:57:55 +0700

    deploy

--- app/core/Model.php
+++ app/core/Model.php
@@ -6,8 +6,8 @@
             $user = $_ENV["DB_USER"];
             $pass = $_ENV["DB_PASS"];
             $data = $_ENV["DB_DATA"];
-            // $db =  new mysqli($host, $user, $pass, $data) or die("There was a problem connecting to the database!");
-            $db =  new mysqli("localhost", "ehc", "ehcteam3", "Classroom") or die("There was a problem connecting to the database!");
+            $db =  new mysqli($host, $user, $pass, $data) or die("There was a problem connecting to the database!");
+            // $db =  new mysqli("localhost", "ehc", "ehcteam3", "Classroom") or die("There was a problem connecting to the database!");
             return $db;
         }
     }
